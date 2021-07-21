0ce555c2dbfce7ecab9043d329fce5aa6f20bbfc
Nathyane Moreno <nathyane.moreno@gmail.com>
2021-07-21 20:51:26 -0300

    write syntax to .xml

--- lib/syntaxer/Syntaxer.ex
+++ lib/syntaxer/Syntaxer.ex
@@ -2,10 +2,25 @@ defmodule Syntaxer do
   def start(fileIn) do
     stream = File.read!(fileIn)
     syntaxResult = SyntaxerNDFA.checkToken(stream, "")
+
+    if(File.exists?(fileIn)) do
+      File.open!(fileIn)
+      # Create fileOut with fileIn basename
+      fileOut = "build/xml/syntax/" <> Path.basename(fileIn, ".jack") <> ".xml"
+      if(File.exists?(fileOut), do: File.rm!(fileOut), else: File.write!(fileOut, ""))
+
+      # Write xml
+      xmler(fileOut, syntaxResult)
+
+      File.close(fileIn)
+      File.close(fileOut)
+    else
+      IO.puts(IO.ANSI.format(IO.ANSI.red() <> "\nFile #{fileIn} not found." <> IO.ANSI.reset()))
+      {:error, "File not found."}
+    end
   end
 
-  def start(fileIn, fileOut) do
-    token = Lexer.start(fileIn, fileOut)
+  def xmler(fileOut, string) do
+    File.write!(fileOut, string)
   end
-
 end
